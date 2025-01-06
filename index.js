019636a4b71cac88adbb0f7d9ac227d5105d7c27
Thiago Ferrer <thi-ferrer@live.com>
2025-01-06 10:38:47 -0300

    Criar elementos e Checkbox

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
 const inputItem = document.getElementById("input-item");
+let contador = 0;
+
+const botaoAdicionar = document.getElementById("adicionar-item");
+botaoAdicionar.addEventListener("click", (evento) => {
+    evento.preventDefault();
+    console.log(inputItem.value);
+    if (inputItem.value == "") {
+        alert("Digite um item para adicionar");
+        return
+    }
+
+    const itemDaLista = document.createElement("li");
+    const containerItemDaLista = document.createElement("div");
+    containerItemDaLista.classList.add("lista-item-container");
+    const inputCheckbox = document.createElement("input");
+    inputCheckbox.type = "checkbox";
+    inputCheckbox.id = "checkbox" + contador ++;
 
 
-console.log(inputItem.valeu);
+});
+
