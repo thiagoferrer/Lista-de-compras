90b73c87b07d291a11d27f63d972bdd77134b526
Thiago Ferrer <thi-ferrer@live.com>
2025-01-06 11:17:26 -0300

    Data completa, Horário

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const inputItem = document.getElementById("input-item");
+const listaDeCompras = document.getElementById("lista-de-compras");
 let contador = 0;
 
 const botaoAdicionar = document.getElementById("adicionar-item");
@@ -15,8 +16,28 @@ botaoAdicionar.addEventListener("click", (evento) => {
     containerItemDaLista.classList.add("lista-item-container");
     const inputCheckbox = document.createElement("input");
     inputCheckbox.type = "checkbox";
-    inputCheckbox.id = "checkbox" + contador ++;
+    inputCheckbox.id = "checkbox-" + contador++;
+    const nomeItem = document.createElement("p");
+    nomeItem.innerText = inputItem.value;
 
+    containerItemDaLista.appendChild(inputCheckbox);
+    containerItemDaLista.appendChild(nomeItem);
 
-});
+    itemDaLista.appendChild(containerItemDaLista);
+    const diaDaSemana = new Date().toLocaleDateString("pt-BR", {
+        weekday: "long",
+    });
+    const data = new Date().toLocaleDateString("pt-BR")
+    const hora = new Date(). toLocaleTimeString("pt-BR", {
+        hour: "numeric",
+        minute: "numeric",
+    })
+    const dataCompleta = `${diaDaSemana} (${data}) às ${hora}`
+    const itemData = document.createElement("p");
+    itemData.innerText = dataCompleta;
+    itemData.classList.add("texto-data");
+    itemDaLista.appendChild(itemData);
+    listaDeCompras.appendChild(itemDaLista);
+    
+})
 
