69a01d108bc099bf02b00cea8629a793d96d16b2
Thiago Ferrer <thi-ferrer@live.com>
2025-01-06 16:40:02 -0300

    Modularização

--- index.js
+++ index.js
@@ -1,43 +1,24 @@
-const inputItem = document.getElementById("input-item");
+import { criarItemDaLista } from "./script/criarItemDaLista.js";
 const listaDeCompras = document.getElementById("lista-de-compras");
-let contador = 0;
-
 const botaoAdicionar = document.getElementById("adicionar-item");
+
+
 botaoAdicionar.addEventListener("click", (evento) => {
     evento.preventDefault();
-    console.log(inputItem.value);
-    if (inputItem.value == "") {
-        alert("Digite um item para adicionar");
-        return
-    }
-
-    const itemDaLista = document.createElement("li");
-    const containerItemDaLista = document.createElement("div");
-    containerItemDaLista.classList.add("lista-item-container");
-    const inputCheckbox = document.createElement("input");
-    inputCheckbox.type = "checkbox";
-    inputCheckbox.id = "checkbox-" + contador++;
-    const nomeItem = document.createElement("p");
-    nomeItem.innerText = inputItem.value;
-
-    containerItemDaLista.appendChild(inputCheckbox);
-    containerItemDaLista.appendChild(nomeItem);
-
-    itemDaLista.appendChild(containerItemDaLista);
-    const diaDaSemana = new Date().toLocaleDateString("pt-BR", {
-        weekday: "long",
-    });
-    const data = new Date().toLocaleDateString("pt-BR")
-    const hora = new Date(). toLocaleTimeString("pt-BR", {
-        hour: "numeric",
-        minute: "numeric",
-    })
-    const dataCompleta = `${diaDaSemana} (${data}) às ${hora}`
-    const itemData = document.createElement("p");
-    itemData.innerText = dataCompleta;
-    itemData.classList.add("texto-data");
-    itemDaLista.appendChild(itemData);
+    const itemDaLista = criarItemDaLista();
     listaDeCompras.appendChild(itemDaLista);
-    
+    verificarListaVazia();
 })
 
+const mensagemListaVazia = document.querySelector(".mensagem-lista-vazia");
+
+function verificarListaVazia() {
+    const itensDalista = listaDeCompras.querySelectorAll("li");
+    if (itensDalista.length === 0) {
+        mensagemListaVazia.style.display = "block";
+    } else {
+        mensagemListaVazia.style.display = "none";
+    }
+}
+
+verificarListaVazia();
